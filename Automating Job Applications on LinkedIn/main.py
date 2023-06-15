98f99f78462a6610eca4b9712fadb20e016da97a
MichaelSolo-py <forprogramming23@gmail.com>
2023-06-15 16:41:25 +0300

    Finish Automating Job Applications on LinkedIn

--- Automating Job Applications on LinkedIn/main.py
+++ Automating Job Applications on LinkedIn/main.py
@@ -1,57 +1,84 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver as uc
 import time
 
-url = "https://www.linkedin.com/jobs/search/?currentJobId=3627380463&f_AL=true&f_E=2%2C3&f_JT=F&keywords=python%20developer&refresh=true&sortBy=R"
-driver = webdriver.Chrome()
-driver.get(url)
+option = uc.ChromeOptions()
+option.add_argument('--user-data-dir=/home/your_username/.config/google-chrome/Default')
 
-login = 'forprogramming23@gmail.com'
-password = ''
-MOBILE = ''
+driver = uc.Chrome(option=option)
+driver.get(
+    "https://www.linkedin.com/jobs/search/?currentJobId=3627380463&f_AL=true&f_E=2%2C3&f_JT=F&keywords=python%20developer&refresh=true&sortBy=R")
+driver.maximize_window()
 
-sing_in_button = driver.find_element(By.LINK_TEXT, 'Sign in')
-sing_in_button.click()
+login = 'EMAIL@gmail.com'
+password = 'PASS'
+MOBILE = 'MOBILE'
 
-time.sleep(2)  # wait until load
+# Click on the "Sign in" button
+sign_in_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Sign in')))
+sign_in_button.click()
 
-input_email = driver.find_element(By.CSS_SELECTOR, '#username')
-input_email.send_keys(login)
+# Fill in email and password fields and click "Sign in"
+try:
+    input_email = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#username')))
+    input_email.send_keys(login)
 
-input_pass = driver.find_element(By.CSS_SELECTOR, '#password')
-input_pass.send_keys(password)
+    input_pass = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#password')))
+    input_pass.send_keys(password)
+    input_pass.send_keys(Keys.ENTER)
+except TimeoutException:
+    print("Email or password input fields not found.")
 
-sing_in_to_acc = driver.find_element(By.CSS_SELECTOR, '.btn__primary--large')
-sing_in_to_acc.click()
+# Scroll to the pagination element
+try:
+    scroll_to_pagination = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#ember294')))
+    driver.execute_script("arguments[0].scrollIntoView(true);", scroll_to_pagination)
+except TimeoutException:
+    print("Pagination element not found.")
 
-time.sleep(2)
+# Get the list of job propositions
+block_vacations = WebDriverWait(driver, 3).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'scaffold-layout__list-container')))
+vacations_propositions = block_vacations.find_elements(By.TAG_NAME, 'a')
 
-# -- SCROLLING --
-scroll_to_pagination = driver.find_element(By.CSS_SELECTOR, '#ember294')
-driver.execute_script("arguments[0].scrollIntoView(true);", scroll_to_pagination)
+# Iterate over the job propositions
+for proposition_link in vacations_propositions:
 
-# -- JOBS LIST --
-vacations_propositions = driver.find_element(By.CLASS_NAME, 'scaffold-layout__list-container')
-test1 = vacations_propositions.find_elements(By.TAG_NAME, 'a')
-time.sleep(1)
-for v in test1:
-    print(v.text)
+    time.sleep(5)
 
-    choose_vac = vacations_propositions.find_element(By.LINK_TEXT, v.text)
-    choose_vac.click()
+    print(proposition_link.text)
 
-    # -- Save and Follow  --
-    save_button = driver.find_element(By.CSS_SELECTOR,
-                                      '#main > div > div.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details > div:nth-child(1) > div > div:nth-child(1) > div > div.relative.jobs-unified-top-card__container--two-pane > div.jobs-unified-top-card__content--two-pane > div:nth-child(4) > div > button')
-    save_button.click()
+    try:
+        choose_vac = driver.find_element(By.LINK_TEXT, proposition_link.text)
+        choose_vac.click()
 
-    wait = WebDriverWait(driver, 10)
-    follow_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'follow')))
-    follow_button.click()
+        time.sleep(5)
 
+        easy_to_apply = driver.find_element(By.CSS_SELECTOR,
+                                            '#main > div > div.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details > div:nth-child(1) > div > div:nth-child(1) > div > div.relative.jobs-unified-top-card__container--two-pane > div.jobs-unified-top-card__content--two-pane > div:nth-child(4) > div > div')
+        easy_to_apply.click()
 
-time.sleep(1000)
+        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+
+        # If the submit_button is a "Next" button, then this is a multistep application, so skip.
+        if submit_button.get_attribute("aria-label") == "Continue to next step":
+            close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_elements(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")[1]
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+    except NoSuchElementException:
+        print(f"Job proposition link '{proposition_link.text}' not found.")
+        continue
+
 driver.quit()
