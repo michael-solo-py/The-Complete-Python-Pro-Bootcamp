4e692c53250d3b327edb21d46061a448bcf80722
MichaelSolo-py <forprogramming23@gmail.com>
2023-07-14 16:52:37 +0300

    try templating with Jinja

---
+++ Blog-start/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from post import Post
+import requests
+
+response = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
+post_arr = []
+for item in response:
+    post_obj = Post(item['id'], item['title'], item['subtitle'], item['body'])
+    post_arr.append(post_obj)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_posts=post_arr)
+
+
+@app.route('/blog/<int:index>')
+def write_post_main(index):
+    requested_post = None
+    for block in post_arr:
+        if block.id == index:
+            requested_post = block
+    return render_template('post.html', post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
